8e47a47fc5ccd4d18c25f5231a8b9e9d555d74f3
ahsan@example.com <ahsan@example.com>
2024-02-21 00:41:11 +0500

    add an edit page for tarsnsforming images

--- src/components/image-menu.tsx
+++ src/components/image-menu.tsx
@@ -1,6 +1,3 @@
-import{ FolderPlus } from "lucide-react"
-  
-  import { Button } from "@/components/ui/button"
   import {
     DropdownMenu,
     DropdownMenuContent,
@@ -12,20 +9,30 @@ import { Menu } from "./icons/menu"
 import { AddToAlumDialog } from "./add-to-album-dialog"
 import { SearchResult } from "@/app/gallery/page"
 import { useState } from "react"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { Pencil } from "lucide-react"
   
   export function ImageMenu({image}:{image: SearchResult}) {
     const [open, setOpen]= useState(false);
+   
     return (
         <div className="absolute top-2 right-2">
       <DropdownMenu open={open} onOpenChange={setOpen}>
         <DropdownMenuTrigger asChild>
-            <Button variant="secondary" className="  w-8 h-8 p-0">
+            <Button variant="secondary" className=" w-8 h-8 p-0">
          <Menu/>
          </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-40">
             <DropdownMenuItem asChild>
              <AddToAlumDialog image={image}  onClose={()=> setOpen(false)}/>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+             <Link href={`/edit?publicId=${encodeURIComponent(image.public_id)}`}>
+              <Pencil className="mr-2 h-4 w-4"/>
+              Edit
+             </Link>
             </DropdownMenuItem>
             </DropdownMenuContent>
             </DropdownMenu>
