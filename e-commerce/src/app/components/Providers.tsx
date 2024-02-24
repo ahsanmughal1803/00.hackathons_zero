4752cc54bf68e0448da18280715fc4f577538c09
ahsan@example.com <ahsan@example.com>
2024-02-24 13:07:09 +0500

    finished project

--- e-commerce/src/app/components/Providers.tsx
+++ e-commerce/src/app/components/Providers.tsx
@@ -9,7 +9,7 @@ export default function CartProvider({children}:{children: ReactNode}){
           mode="payment" 
           cartMode="client-only" 
           stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-          successUrl="https://localhost:3000/atripe/success"
+          successUrl="https://localhost:3000/stripe/success"
           cancelUrl="https://localhost:3000/stripe/error"
           currency="USD"
           billingAddressCollection={false}
