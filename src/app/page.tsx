b57f60b9ba80127e3b9cc7d52e766bd90d9115a1
ahsan@example.com <ahsan@example.com>
2024-03-06 20:51:57 +0500

    everything should work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import Newest from "./components/Newest";
-
 export const dynamic = "force-dynamic";
 
 export default function Home() {
